4fa8375f1e064c524156e2e7b6190f4c3bf56749
Jonilson Sousa <jonilsonds9@gmail.com>
2024-02-07 20:30:59 -0300

    Add logger e fixes

--- src/infra/services/payment.service.impl.ts
+++ src/infra/services/payment.service.impl.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { HttpClientService } from './http-client.service';
 import { PaymentService } from '../../domain/services/payment.service';
 import { Pedido } from '../../domain/model/pedido';
@@ -7,16 +7,23 @@ import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PaymentServiceImpl implements PaymentService {
+  private readonly logger = new Logger(PaymentServiceImpl.name);
   constructor(
     private readonly httpClientService: HttpClientService,
     private readonly configService: ConfigService,
   ) {}
 
+  /*
+    Precisa de um serviço de pagamento para enviar o pedido para pagamento
+   */
   async sendOrderToPayment(pedido: Pedido): Promise<void> {
-    const paymentUrl = this.configService.get('PAYMENT_SERVICE_URL');
-    await this.httpClientService.post(
-      `${paymentUrl}/api/pagamentos/novo`,
-      new PedidoPresenter(pedido),
+    const paymentUrl = this.configService.get('PAYMENTS_SERVICE_URL');
+    const url = `${paymentUrl}/api/pagamentos/novo`;
+
+    this.logger.log(
+      `[Sender] Enviando pedido com Id [${pedido.id}] para o serviço de pagamentos [${paymentUrl}]`,
     );
+
+    await this.httpClientService.post(url, new PedidoPresenter(pedido));
   }
 }
