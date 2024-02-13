1cc41cf4167df3b863ad3e8189339e4a267f328d
Jonilson Sousa <jonilsonds9@gmail.com>
2024-02-13 18:57:02 -0300

    Fix teste OrdersForPaymentClientFactory

--- src/infra/queues/orders-for-payment-client-factory.service.spec.ts
+++ src/infra/queues/orders-for-payment-client-factory.service.spec.ts
@@ -5,17 +5,24 @@ import { mock, instance, when } from 'ts-mockito';
 import { OrdersForPaymentClientFactory } from './orders-for-payment-client-factory.service';
 
 describe('OrdersForPaymentClientFactory', () => {
-  const userTest = 'user';
-  const passwordTest = 'password';
   let factory: OrdersForPaymentClientFactory;
   let configService: ConfigService;
 
   beforeEach(async () => {
+    process.env.QUEUE_USER = 'user';
+    process.env.QUEUE_PASSWORD = 'password';
+    process.env.QUEUE_HOST = 'localhost';
+    process.env.QUEUE_PORT = '5672';
+
     configService = mock(ConfigService);
-    when(configService.get('QUEUE_USER')).thenReturn(userTest);
-    when(configService.get('QUEUE_PASSWORD')).thenReturn(passwordTest);
-    when(configService.get('QUEUE_HOST')).thenReturn('localhost');
-    when(configService.get('QUEUE_PORT')).thenReturn(5672);
+    when(configService.get('QUEUE_USER')).thenReturn(process.env.QUEUE_USER);
+    when(configService.get('QUEUE_PASSWORD')).thenReturn(
+      process.env.QUEUE_PASSWORD,
+    );
+    when(configService.get('QUEUE_HOST')).thenReturn(process.env.QUEUE_HOST);
+    when(configService.get('QUEUE_PORT')).thenReturn(
+      parseInt(process.env.QUEUE_PORT),
+    );
 
     const module: TestingModule = await Test.createTestingModule({
       providers: [
@@ -40,7 +47,9 @@ describe('OrdersForPaymentClientFactory', () => {
       expect(clientOptions).toEqual({
         transport: Transport.RMQ,
         options: {
-          urls: [`amqp://${userTest}:${passwordTest}@localhost:5672`],
+          urls: [
+            `amqp://${process.env.QUEUE_USER}:${process.env.QUEUE_PASSWORD}@${process.env.QUEUE_HOST}:${process.env.QUEUE_PORT}`,
+          ],
           queue: 'pedidos_para_pagamentos',
           queueOptions: {
             durable: true,
@@ -48,5 +57,12 @@ describe('OrdersForPaymentClientFactory', () => {
         },
       });
     });
+  });
+
+  afterEach(() => {
+    delete process.env.QUEUE_USER;
+    delete process.env.QUEUE_PASSWORD;
+    delete process.env.QUEUE_HOST;
+    delete process.env.QUEUE_PORT;
   });
 });
